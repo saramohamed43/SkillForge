8ef6dcb1d1665d11983884cbb3c0c47fdb066ab1
sarazaghlool <es.sara.ahmed2023@alexu.edu.eg>
2025-11-15 15:07:45 +0200

    fixed lesson class

--- SkillForge/src/skillforge/Lesson.java
+++ SkillForge/src/skillforge/Lesson.java
@@ -2,53 +2,31 @@ package skillforge;
 import java.util.ArrayList;
 
 public class Lesson {
+
     private String lessonID;
     private String lessonTitle;
     private String lessonContent;
-    private ArrayList<String> optionalRresources;
+    private ArrayList<String> optionalResources;
 
-    //instructor with resources
     public Lesson(String lessonID, String lessonTitle, String lessonContent) {
         this.lessonID = lessonID;
         this.lessonTitle = lessonTitle;
         this.lessonContent = lessonContent;
-        this.optionalRresources = new ArrayList<>();
-    }
-    
-    public ArrayList<String> getOptionalRresources() {
-        return optionalRresources;
-    }
-    
-    public String getLessonID() {
-        return lessonID;
+        this.optionalResources = new ArrayList<>();
     }
 
-    public String getLessonTitle() {
+    public String getLessonID() { return lessonID; }
+    public String getLessonTitle() { return lessonTitle; }
+    public String getLessonContent() { return lessonContent; }
+    public ArrayList<String> getOptionalResources() { return optionalResources; }
+
+    public void setLessonID(String lessonID) { this.lessonID = lessonID; }
+    public void setLessonTitle(String lessonTitle) { this.lessonTitle = lessonTitle; }
+    public void setLessonContent(String lessonContent) { this.lessonContent = lessonContent; }
+    public void setOptionalResources(ArrayList<String> optionalResources) { this.optionalResources = optionalResources; }
+
+    @Override
+    public String toString() {
         return lessonTitle;
-    }
-
-    public String getLessonContent() {
-        return lessonContent;
-    }
-
-
-    public void setOptionalRresources(ArrayList<String> optionalRresources) {
-        this.optionalRresources = optionalRresources;
-    }
-
-    public void setLessonID(String lessonID) {
-        this.lessonID = lessonID;
-    }
-
-    public void setLessonTitle(String lessonTitle) {
-        this.lessonTitle = lessonTitle;
-    }
-
-    public void setLessonContent(String lessonContent) {
-        this.lessonContent = lessonContent;
-    }
-    
-    public void saveProgress(Student student){
-        String courseID = 
     }
 }
