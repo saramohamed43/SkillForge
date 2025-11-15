bce19c4ea7177d123794a54e497c657b13d6b656
Bassantzidan <es.bassant.salah2023@alexu.edu.eg>
2025-11-15 13:21:21 +0200

    User Abstract class

--- SkillForge/src/skillforge/User.java
+++ SkillForge/src/skillforge/User.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package skillforge;
 
-/**
- *
- * @author SHADY
- */
-public class User {
-    
+public abstract class User{
+    public int userId;
+    public String role;
+    public String username;
+    public String email;
+    public String passwordHash;
+
+    public User(int userId, String role, String username, String email, String passwordHash) {
+        this.userId = userId;
+        this.role = role;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }  
 }
