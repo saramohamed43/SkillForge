e6aed1ccdce1f2bea312d3738cfbb65c9c7ef09f
Bassantzidan <es.bassant.salah2023@alexu.edu.eg>
2025-11-15 13:20:55 +0200

    Student class

--- SkillForge/src/skillforge/Student.java
+++ SkillForge/src/skillforge/Student.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package skillforge;
 
-/**
- *
- * @author SHADY
- */
-public class Student {
-    
+import java.util.ArrayList;
+
+public class Student extends User{
+    private ArrayList<Integer> enrolledCourses = new ArrayList<>();
+
+    public Student(int userId, String role, String username, String email, String passwordHash) {
+        super(userId, role, username, email, passwordHash);
+    }
+
+    public ArrayList<Integer> getEnrolledCourses() {
+        return enrolledCourses;
+    }
 }
+
