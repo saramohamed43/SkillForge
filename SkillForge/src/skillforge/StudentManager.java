bb636d3d4f65b69ca3603aea24724ce334912a76
zeinaelbanna <zeinaelbanna5@gmail.com>
2025-11-15 13:15:49 +0200

    created enrolled courses methods

--- SkillForge/src/skillforge/StudentManager.java
+++ SkillForge/src/skillforge/StudentManager.java
@@ -9,5 +9,31 @@ package skillforge;
  * @author zmezm
  */
 public class StudentManager {
+    private final ServiceDatabase serviceDatabase;
     
+
+public StudentManager(ServiceDatabase serviceDatabase){
+this.serviceDatabase=serviceDatbase;
+}
+
+public List<Course> getAllCourses(){
+return serviceDatabase.loadAllCourses;
+}
+public List<Course> getEnrolledCourses(Student student){
+    List<Course> allCourses = serviceDatabase.loadAllCourses;
+    List<Course> enrolled =new ArrayList<>();
+    for(Course c:allCourses){
+        if (student.getEnrolledCourses().contains(c.getCourseID()))
+            enrolled.add(c);
+        
+    }
+    return enrolled;
+}
+public boolean enrollStudentInCourse(Student student,Course course){
+   if (student.getEnrolledCourses.contains(course.getCourseID())) 
+       return false;
+   
+   
+}
+       
 }
