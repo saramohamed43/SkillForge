a3da8d2eb8ca595ec32ac7b19a163031db2c8eb2
Bassantzidan <es.bassant.salah2023@alexu.edu.eg>
2025-11-15 16:19:26 +0200

    Update UserAuth .java

--- SkillForge/src/skillforge/UserAuth.java
+++ SkillForge/src/skillforge/UserAuth.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package skillforge;
 
-/**
- *
- * @author SHADY
- */
+import java.util.UUID;
+import static skillforge.User.hashPassword;
+
 public class UserAuth {
-    
+    public static boolean signup(String username,String email,String password,String role){
+        if(username.isEmpty()|| email.isEmpty() || password.isEmpty())
+            return false;
+        if(!email.contains("@")&&!email.contains("."))
+            return false;
+        if (JsonDatabaseManager.emailExists(email)) return false;
+         
+       String userId=UUID.randomUUID().toString().substring(0,8).toUpperCase();
+       String passwordHash= hashPassword(password);
+       User newUser;
+       if(role.equalsIgnoreCase("Student")){
+           newUser=new Student(userId,username,email,passwordHash);
+           
+       }else if(role.equalsIgnoreCase("Instructor")){
+           newUser=new Instructor(userId, username, email, passwordHash);
+       }else{
+           return false;
+       }
+           return JsonDatabaseManager.saveUser(newUser);
+       }
+     public static User login(String email,String password){
+         if(email.isEmpty()&& password.isEmpty())
+             return null;
+         User user=JsonDatabaseManager.getUserByEmail(email);
+         if(user==null)
+             return null;
+         String hashedPassword= hashPassword(password);
+         if(!user.getPasswordHash().equals(hashedPassword))
+             return null;
+         return user;
+     }   
 }
