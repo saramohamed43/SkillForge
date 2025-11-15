f86d6dede2f5bb3b0b30adbdc70b0a1a172731c1
Bassantzidan <es.bassant.salah2023@alexu.edu.eg>
2025-11-15 15:13:48 +0200

    updated class Student and User

--- SkillForge/src/skillforge/Student.java
+++ SkillForge/src/skillforge/Student.java
@@ -1,16 +1,30 @@
 package skillforge;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Student extends User{
-    private ArrayList<Integer> enrolledCourses = new ArrayList<>();
+    private ArrayList<String> enrolledCourses = new ArrayList<>();
+    private Map<String,Integer> progress; 
 
     public Student(int userId, String role, String username, String email, String passwordHash) {
         super(userId, role, username, email, passwordHash);
+        this.enrolledCourses=new ArrayList<>();
+        this.progress = new HashMap<>();
+    }
+    public void enrollInCourse (String courseId){
+        if(!enrolledCourses.contains(courseId)){
+           enrolledCourses.add(courseId);
+            progress.put(courseId,0);
+        }
+    }
+    public ArrayList<String> getEnrolledCourses() {
+        return enrolledCourses;
     }
 
-    public ArrayList<Integer> getEnrolledCourses() {
-        return enrolledCourses;
+    public Map<String, Integer> getProgress() {
+        return progress;
     }
 }
 
